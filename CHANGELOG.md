341005c64db453746e3dda97251023ba479656d1
Brian Takita <brian.takita@gmail.com>
2023-12-14 04:52:13 -0500

    # rebuildjs
    ## 0.15.1
    
    ### Patch Changes
    
    - asset*path_a*: fix: type
    
    ## 0.15.0
    
    ### Minor Changes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # rebuildjs
 
+## 0.15.1
+
+### Patch Changes
+
+- asset*path_a*: fix: type
+
 ## 0.15.0
 
 ### Minor Changes
--- asset/index.d.ts
+++ asset/index.d.ts
@@ -1,4 +1,6 @@
 export declare function asset_path_(mod_promise:Promise<{ default:string }>):Promise<string>
 export declare function asset_path_a_<
-	arg_a_T extends readonly Promise<{ default:string }>[]
->(...mod_promise_a:arg_a_T):Promise<{ [key:keyof arg_a_T]:arg_a_T[key] }>
+	Tuple extends [...Promise<{ default:string }>[]]
+>(...mod_promise_a:Tuple):Promise<{
+	[Index in keyof Tuple]: string
+}>
