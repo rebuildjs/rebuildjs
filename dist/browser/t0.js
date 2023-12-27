6b61624e633ab40bf03d727a759aa066a1b01a43
Brian Takita <brian.takita@gmail.com>
2023-12-27 00:41:53 -0500

    dist/browser/t0.js: import { file_exists_ } from 'ctx-core/fs'

--- dist/browser/t0.js
+++ dist/browser/t0.js
@@ -1,8 +1,8 @@
 // fail
-// import { file_exists_ } from "ctx-core/fs";
+import { file_exists_ } from "ctx-core/fs";
 // fail
 // import { file_exists_ } from "./all/file_exists/index.js";
 // fail
 // import { file_exists_ } from "../../node_modules/ctx-core/all/file_exists/index.js";
 // fail
-import { file_exists_ } from "../../../ctx-core/all/file_exists/index.js";
+// import { file_exists_ } from "../../../ctx-core/all/file_exists/index.js";
