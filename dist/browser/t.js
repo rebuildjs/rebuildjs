004e954c831d15163d0907a7b77f8eda06210149
Brian Takita <brian.takita@gmail.com>
2023-12-27 00:58:28 -0500

    dist/browser/t.js: + esmock.p('./copy/file_exists/index.js', ...): success

--- dist/browser/t.js
+++ dist/browser/t.js
@@ -3,6 +3,8 @@ import esmock from 'esmock'
 // await esmock.p('./index.js', import.meta.url, {}, {
 // success
 // await esmock.p('./all/file_exists/index.js', import.meta.url, {}, {
+// success
+// await esmock.p('./copy/file_exists/index.js', import.meta.url, {}, {
 // fail
 await esmock.p('./t0.js', import.meta.url, {}, {
 	'node:fs/promises': {
