1950a6109cfb0574effa6a700bc868117b1a3bed
Brian Takita <brian.takita@gmail.com>
2023-12-27 00:29:45 -0500

    annotating fail & success

--- dist/browser/t.js
+++ dist/browser/t.js
@@ -1,6 +1,9 @@
 import esmock from 'esmock'
+// fail
 // await esmock.p('./index.js', import.meta.url, {}, {
+// success
 // await esmock.p('./all/file_exists/index.js', import.meta.url, {}, {
+// fail
 await esmock.p('./t0.js', import.meta.url, {}, {
 	'node:fs/promises': {
 		access: 'ESMOCK_access',
--- dist/browser/t0.js
+++ dist/browser/t0.js
@@ -1,3 +1,6 @@
+// fail
 // import { file_exists_ } from "ctx-core/fs";
+// fail
 import { file_exists_ } from "./all/file_exists/index.js";
+// fail
 // import { file_exists_ } from "../../node_modules/ctx-core/all/file_exists/index.js";
